224edeaf9137ef6f44964219256fbd116cb93e1a
keigo.kunitomo <keigo1573762@icloud.com>
2020-07-04 10:42:51 +0000

    user login

--- app/User.php
+++ app/User.php
@@ -37,11 +37,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
- 
 }
-
-User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
