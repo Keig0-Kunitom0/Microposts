7ea850866a9ea93309c0ac4f5dfd94cf45b102d9
keigo.kunitomo <keigo1573762@icloud.com>
2020-07-07 18:28:20 +0000

    post

--- app/User.php
+++ app/User.php
@@ -37,4 +37,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
 }
